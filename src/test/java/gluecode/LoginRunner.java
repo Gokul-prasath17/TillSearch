f915ca1fec9727aa722db8132215118ba9845c24
Gokul-prasath17 <32924902+Gokul-prasath17@users.noreply.github.com>
2019-07-23 17:58:53 +0530

    pushing files till search

---
+++ src/test/java/gluecode/LoginRunner.java
@@ -0,0 +1,14 @@
+package gluecode;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"src/test/resources/Features/case1.feature","src/test/resources/Features/case2.feature","src/test/resources/Features/case3.feature"},plugin="json:target/Loginreport1705.json")
+//@CucumberOptions(features= "src/test/resources/Features/case3.feature",plugin="json:target/Loginreport28.json")
+
+public class LoginRunner {
+
+}
